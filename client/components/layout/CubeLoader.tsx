30ae280a0d7b5042b3d0549aa428c83b686c16c0
sahilsheestha <118797970+sahilsheestha@users.noreply.github.com>
2025-07-20 23:50:45 +0545

    changes in layout

--- client/components/layout/CubeLoader.tsx
+++ client/components/layout/CubeLoader.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 const CubeLoader = () => {
     return (
         <div className="sk-cube-grid">
-         k-cube6"></div>
+            <div className="sk-cube sk-cube1"></div>
+            <div className="sk-cube sk-cube2"></div>
+            <div className="sk-cube sk-cube3"></div>
+            <div className="sk-cube sk-cube4"></div>
+            <div className="sk-cube sk-cube5"></div>
+            <div className="sk-cube sk-cube6"></div>
             <div className="sk-cube sk-cube7"></div>
             <div className="sk-cube sk-cube8"></div>
             <div className="sk-cube sk-cube9"></div>
